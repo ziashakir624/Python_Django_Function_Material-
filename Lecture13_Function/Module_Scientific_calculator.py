75964e063707d50a525329d5adb298f6931b64a9
ZiaShakir <127202068+ziashakir624@users.noreply.github.com>
2023-09-20 14:56:20 +0500

    Module_Scientific_calculator

---
+++ Lecture13_Function/Module_Scientific_calculator.py
@@ -0,0 +1,15 @@
+def addition(a,b):
+    return (a+b)
+
+
+def subtraction(a,b):
+    return(a-b)
+
+def multiply(a,b):
+    return(a*b)
+
+def division(a,b):
+    if b == 0:
+        print("Sorry! cannot divide by Zero.")
+    else:
+        return(a/b)
